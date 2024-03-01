a41f7954d21b9998ec1bb8537ee7eec821bffc62
maria ben mohamed <mariabenmohamed01@gmail.com>
2024-03-01 17:50:26 +0100

    first commit here

--- App.js
+++ App.js
@@ -3,17 +3,15 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>home screen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <text>rayan's part</text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
 });
